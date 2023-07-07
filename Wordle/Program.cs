26ba9f7d4c66f04045ac5460f884b2d745861cb6
Emanuele Moncada <em@zerowk.it>
2023-07-07 17:32:00 +0200

    feat(Program): import file && cath FileNotFound exeption

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Tic_Tac_Toe;
+﻿using Wordle_Library;
+
+namespace Tic_Tac_Toe;
 
 internal class Program
 {
 
     static public void Main()
     {
+        const string FilePath = @"C:\Users\EmanueleMoncada\Desktop\fakedb\wordTable.txt";
+        try
+        {
+            List<string> wordlist = File.ReadAllLines(FilePath).ToList();
+            Logic game = new(wordlist);
+            game.GetList();
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found! IDIOT");
+        }
 
     }
 }

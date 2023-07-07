b7edcbd1c65999d93d677e659a147d68dde1b35a
Emanuele Moncada <em@zerowk.it>
2023-07-07 17:31:13 +0200

    feat(Logic): Initialize dll class

--- Wordle-Library/Logic.cs
+++ Wordle-Library/Logic.cs
@@ -2,6 +2,19 @@
 {
     public class Logic
     {
+        readonly List<string> listword = new List<string>();
+        public Logic(List<string> word)
+        {
+            this.listword = word;
+        }
 
+        // Test Method to check correct open file
+        public void GetList()
+        {
+            foreach (string word in listword)
+            {
+                Console.WriteLine(word);
+            }
+        }
     }
 }

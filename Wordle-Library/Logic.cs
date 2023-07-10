4db72ba5c9d8c1a8ee0a8b48c175788ce854c88f
Emanuele Moncada <em@zerowk.it>
2023-07-10 09:26:51 +0200

    feat(project): move method for print list in Program.cs

--- Wordle-Library/Logic.cs
+++ Wordle-Library/Logic.cs
@@ -2,19 +2,14 @@
 {
     public class Logic
     {
-        readonly List<string> listword = new List<string>();
+        private readonly List<string> _listword = new List<string>();
         public Logic(List<string> word)
         {
-            this.listword = word;
+            this._listword = word;
         }
 
-        // Test Method to check correct open file
-        public void GetList()
-        {
-            foreach (string word in listword)
-            {
-                Console.WriteLine(word);
-            }
-        }
+        public List<string> Listword { get { return _listword; } }
+
+
     }
 }
--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -5,6 +5,14 @@ namespace Tic_Tac_Toe;
 internal class Program
 {
 
+    public static void PrintList(List<string> listToPrint)
+    {
+        foreach (string word in listToPrint)
+        {
+            Console.WriteLine(word);
+        }
+    }
+
     static public void Main()
     {
         const string FilePath = @"C:\Users\EmanueleMoncada\Desktop\fakedb\wordTable.txt";
@@ -12,11 +20,20 @@ internal class Program
         {
             List<string> wordlist = File.ReadAllLines(FilePath).ToList();
             Logic game = new(wordlist);
-            game.GetList();
+            PrintList(wordlist);
         }
         catch (FileNotFoundException)
         {
             Console.WriteLine("File not found! IDIOT");
+        }
+        catch (Exception e)
+        {
+            //Catch other Exception
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Finish");
         }
 
     }

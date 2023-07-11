462a139ccae89f5d2abbbb3e75eb52043055fda6
Emanuele Moncada <em@zerowk.it>
2023-07-11 14:42:22 +0200

    fix(Program): fix namaspace

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -1,109 +1,110 @@
 ﻿using Wordle_Library;
 
-namespace Tic_Tac_Toe;
-
-internal class Program
+namespace Wordle
 {
-
-    public static void PrintList(List<string> listToPrint)
+    internal class Program
     {
-        foreach (string word in listToPrint)
-        {
-            Console.WriteLine(word);
-        }
-    }
 
-    public static void PrintGameGrid(char[,] matrix, int wordLength)
-    {
-        for (int i = 0; i < Logic.MaxTurn; i++)
+        public static void PrintList(List<string> listToPrint)
         {
-            for (int j = 0; j < wordLength; j++)
+            foreach (string word in listToPrint)
             {
-
-                if (matrix[i, j] != Logic.InitilizedMatrixSymbol && matrix[i, j] == Char.ToUpper(matrix[i, j]))
-                {
-                    Console.Write($"[\x1b[1;32m{matrix[i, j]}\x1b[1;0m]");
-                }
-                else
-                {
-                    Console.Write($"[{matrix[i, j]}]");
-                }
+                Console.WriteLine(word);
             }
-            Console.WriteLine();
         }
-    }
 
-    static public void Main()
-    {
-        const string FilePath = @"C:\Users\EmanueleMoncada\Desktop\fakedb\wordTable.txt";
-        int turn = Logic.MaxTurn;
-        string winnerWord = "";
-        try
+        public static void PrintGameGrid(char[,] matrix, int wordLength)
         {
-            List<string> wordlist = File.ReadAllLines(FilePath).ToList();
-            Logic game = new(wordlist);
-            winnerWord = game.ChooseRandomWord();
-            char[,] matrix = game.CreateGameMatrix();
-            int attempt = 0;
-            Console.WriteLine($"DEBUG - CORRECT WORD [{winnerWord}]");
-            Console.WriteLine($"Word have [{game.WordLength}] letter");
-            while (turn > 0)
+            for (int i = 0; i < Logic.MaxTurn; i++)
             {
-                Console.Write("Insert phrase: ");
-                string? toSend = Console.ReadLine();
-                if (toSend != null)
+                for (int j = 0; j < wordLength; j++)
                 {
-                    toSend = toSend.Trim().ToLower().Replace(" ", "");
-                }
 
-                if (toSend != null && toSend.Length == game.WordLength)
-                {
-                    game.InserIntoMatrix(attempt, toSend, matrix);
-                    turn--;
-                    attempt++;
-                    Console.Clear();
-                    PrintGameGrid(matrix, winnerWord.Length);
-
-                    if (game.ExistValue.Count > 0)
+                    if (matrix[i, j] != Logic.InitilizedMatrixSymbol && matrix[i, j] == Char.ToUpper(matrix[i, j]))
                     {
-                        Console.Write("Value ");
-                        foreach (char sentence in game.ExistValue)
-                        {
-                            Console.Write($"[\u001b[1;33m{sentence.ToString().ToUpper()}\u001b[1;0m]");
-
-                        }
-                        Console.WriteLine(" exist but the position is wrong");
+                        Console.Write($"[\x1b[1;32m{matrix[i, j]}\x1b[1;0m]");
                     }
-                    Console.WriteLine($"Word have [{game.WordLength}] letter");
-                    if (game.IsWinner(toSend))
+                    else
                     {
-                        turn = 0;
+                        Console.Write($"[{matrix[i, j]}]");
                     }
                 }
-                else
+                Console.WriteLine();
+            }
+        }
+
+        static public void Main()
+        {
+            const string FilePath = @"C:\Users\EmanueleMoncada\Desktop\fakedb\wordTable.txt";
+            int turn = Logic.MaxTurn;
+            string winnerWord = "";
+            try
+            {
+                List<string> wordlist = File.ReadAllLines(FilePath).ToList();
+                Logic game = new(wordlist);
+                winnerWord = game.ChooseRandomWord();
+                char[,] matrix = game.CreateGameMatrix();
+                int attempt = 0;
+                Console.WriteLine($"DEBUG - CORRECT WORD [{winnerWord}]");
+                Console.WriteLine($"Word have [{game.WordLength}] letter");
+                while (turn > 0)
                 {
+                    Console.Write("Insert phrase: ");
+                    string? toSend = Console.ReadLine();
                     if (toSend != null)
                     {
-                        Console.Write($"Length Error - The word entered is too ");
-                        Console.WriteLine(toSend.Length > winnerWord.Length ? "Long" : "Short");
+                        toSend = toSend.Trim().ToLower().Replace(" ", "");
                     }
 
-                }
-            };
-        }
-        catch (FileNotFoundException)
-        {
-            Console.WriteLine("File not found! IDIOT");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
-        finally
-        {
-            Console.WriteLine("Finish");
-            Console.WriteLine($"Correct word is {winnerWord}");
-        }
+                    if (toSend != null && toSend.Length == game.WordLength)
+                    {
+                        game.InserIntoMatrix(attempt, toSend, matrix);
+                        turn--;
+                        attempt++;
+                        Console.Clear();
+                        PrintGameGrid(matrix, winnerWord.Length);
 
+                        if (game.ExistValue.Count > 0)
+                        {
+                            Console.Write("Value ");
+                            foreach (char sentence in game.ExistValue)
+                            {
+                                Console.Write($"[\u001b[1;33m{sentence.ToString().ToUpper()}\u001b[1;0m]");
+
+                            }
+                            Console.WriteLine(" exist but the position is wrong");
+                        }
+                        Console.WriteLine($"Word have [{game.WordLength}] letter");
+                        if (game.IsWinner(toSend))
+                        {
+                            turn = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (toSend != null)
+                        {
+                            Console.Write($"Length Error - The word entered is too ");
+                            Console.WriteLine(toSend.Length > winnerWord.Length ? "Long" : "Short");
+                        }
+
+                    }
+                };
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found! IDIOT");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finish");
+                Console.WriteLine($"Correct word is {winnerWord}");
+            }
+
+        }
     }
 }

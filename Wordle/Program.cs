d2d8c817105d61edd50346d53b2133115f9d52bc
Emanuele Moncada <em@zerowk.it>
2023-07-20 11:37:35 +0200

    feat(Program): log exeption

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -1,9 +1,13 @@
-﻿using Wordle_Library;
+﻿using NLog;
+using System.Configuration;
+using Wordle_Library;
 
 namespace Wordle
 {
     internal class Program
     {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
         public static void PrintGameBoard(List<string> listToPrint)
         {
             Console.WriteLine("\x1b[1m\u001b[1;32mWO\u001b[1;31mRD\u001b[1;33mLE\u001b[1;0m\n");
@@ -28,69 +32,76 @@ namespace Wordle
         static public void Main()
         {
             Console.Title = "Wordle Game";
-            const string FilePath = @"C:\Users\EmanueleMoncada\Desktop\fakedb\wordTable.txt";
+            string? path = ConfigurationManager.AppSettings["FilePath"];
             int turn = Logic.MaxTurn;
-            string winnerWord = "";
+            string winnerWord;
             bool winning = false;
-            try
+            if (path != null)
             {
-                List<string> wordlist = File.ReadAllLines(FilePath).ToList();
 
-                if (wordlist.Count == 0)
+                try
                 {
-                    Console.WriteLine("\u001b[1;31mError - The Collection is empty\x1b[1;0m");
-                    Environment.Exit(1);
-                }
-                Logic game = new(wordlist);
-                winnerWord = game.ChooseRandomWord();
-                Console.WriteLine(winnerWord);
-                Console.WriteLine($"Word have [{game.WordLength}] letter");
-                while (turn > 0)
-                {
-                    Console.Write("Insert phrase: ");
-                    string? toSend = Console.ReadLine();
-                    if (toSend != null)
+                    List<string> wordlist = File.ReadAllLines(path).ToList();
+
+                    if (wordlist.Count == 0)
                     {
-                        toSend = toSend.Trim().ToLower().Replace(" ", "");
-                        if (toSend.Length == game.WordLength)
+                        Console.WriteLine("\u001b[1;31mCould not start the game because of an unexpected error, please contact the technical support.\x1b[1;0m");
+                        logger.Error("LOG - The collection is empty");
+                        Environment.Exit(1);
+                    }
+                    Logic game = new(wordlist);
+                    winnerWord = game.ChooseRandomWord();
+                    Console.WriteLine(winnerWord);
+                    Console.WriteLine($"Word have [{game.WordLength}] letter");
+                    while (turn > 0)
+                    {
+                        Console.Write("Insert phrase: ");
+                        string? toSend = Console.ReadLine();
+                        if (toSend != null)
                         {
-                            game.InsertWord(toSend);
-                            turn--;
-                            Console.Clear();
-                            PrintGameBoard(game.GameBoard);
-                            if (game.ExistValue.Count > 0)
+                            toSend = toSend.Trim().ToLower().Replace(" ", "");
+                            if (toSend.Length == game.WordLength)
                             {
-                                Console.Write("Value ");
-                                foreach (char sentence in game.ExistValue)
+                                game.InsertWord(toSend);
+                                turn--;
+                                Console.Clear();
+                                PrintGameBoard(game.GameBoard);
+                                if (game.ExistValue.Count > 0)
                                 {
-                                    Console.Write($"[\u001b[1;33m{sentence.ToString().ToUpper()}\u001b[1;0m]");
+                                    Console.Write("Value ");
+                                    foreach (char sentence in game.ExistValue)
+                                    {
+                                        Console.Write($"[\u001b[1;33m{sentence.ToString().ToUpper()}\u001b[1;0m]");
 
+                                    }
+                                    Console.WriteLine(" exist but the position is wrong");
                                 }
-                                Console.WriteLine(" exist but the position is wrong");
+                            }
+                            else
+                            {
+                                Console.Write($"\u001b[1;31mLength Error - The word entered is too ");
+                                Console.WriteLine(toSend.Length > winnerWord.Length ? "Long\u001b[1;0m" : "Short\u001b[1;0m");
+                            }
+                            if (game.IsWinner(toSend))
+                            {
+                                turn = 0;
+                                winning = true;
                             }
                         }
-                        else
-                        {
-                            Console.Write($"\u001b[1;31mLength Error - The word entered is too ");
-                            Console.WriteLine(toSend.Length > winnerWord.Length ? "Long\u001b[1;0m" : "Short\u001b[1;0m");
-                        }
-                        if (game.IsWinner(toSend))
-                        {
-                            turn = 0;
-                            winning = true;
-                        }
-                    }
-                };
+                    };
+                    Console.WriteLine(winning ? "\n\u001b[1;32mYou Win\u001b[1;0m" : $"\n\u001b[1;31mI'm sorry but you lose!\nCorrect word is \u001b[1;0m\u001b[1;96m{winnerWord}\u001b[1;0m");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    logger.Error($"LOG - {e.Message}");
+                }
             }
-            catch (FileNotFoundException)
+            else
             {
-                Console.WriteLine("File not found! IDIOT");
+                Console.WriteLine("Specify path in app.setting");
+                logger.Error($"LOG - Path in app.setting is null");
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            Console.WriteLine(winning ? "\n\u001b[1;32mYou Win\u001b[1;0m" : $"\n\u001b[1;31mI'm sorry but you lose!\nCorrect word is \u001b[1;0m\u001b[1;96m{winnerWord}\u001b[1;0m");
         }
     }
 }
